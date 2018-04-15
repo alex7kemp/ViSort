5631d44d7efc71a4016221674f7182f7353db9dc
ThomasLalo <37088796+ThomasLalo@users.noreply.github.com>
2018-04-14 20:17:58 -0500

    Add files via upload

--- visort/countingSort.py
+++ visort/countingSort.py
@@ -1,55 +1,77 @@
 import time
+import psutil
+import os
+import copy
 
-def countingSortSteps(stepsList,countList):
-    aList = stepsList[0]
-    maxplus = max(aList) + 1
-    count = [0] * maxplus
-    countList.append(list(count))
+# the first index of steps list should contain the list you want to sort
+def countingSortSteps(stepsList,indexList,pseudoList):
+
+    changedIndices = [0,0]
+    aList = list(stepsList[0]) #creating a copy of the list
+    maxplus = max(aList) + 1 #this is used to get the correct range of numbers to iterate through
+    count = [0] * maxplus #prefilling count list
+
+    
+    pseudoList.append("insert pseudocode step 1 here")
+    stepsList.append(list(aList))
+    indexList.append(list(changedIndices))
     
     for number in aList:
-        count[number] += 1
-        countList.append(list(count))
-
+        count[number] += 1 #creating count list
+        
+        pseudoList.append("insert pseudocode step 2 here")
+        stepsList.append(list(aList))
+        indexList.append(list(changedIndices))
     
     index = 0
     for value in range(maxplus):
-        for number in range(count[value]): 
-            aList[index] = value
-            stepsList.append(list(aList))
-            index += 1
+        
+        pseudoList.append("insert pseudocode step 3 here")
+        stepsList.append(list(aList))
+        indexList.append(list(changedIndices))
+        
+        for number in range(count[value]):
 
-def countingSortTime(aList):
-    start = time.clock()
+            aList[index] = value #actual sorting
+            
+            pseudoList.append("insert pseudocode step 4 here")
+            stepsList.append(list(aList))  #creating steps list
+            if index > 0:  # creating index list
+                changedIndices = [index-1,index]
+                indexList.append(list(changedIndices))
+            else:
+                changedIndices = [0,0]
+                indexList.append(list(changedIndices))
+                
+            index += 1  #this is for iterating through count list
+            
+            pseudoList.append("insert pseudocode step 5 here")
+            stepsList.append(list(aList))
+            indexList.append(list(changedIndices))
+                
+def countingSort(aList):
+    
     maxplus = max(aList) + 1
     count = [0] * maxplus
     
     for number in aList:
         count[number] += 1
 
-    
     index = 0
     for value in range(maxplus):
         for c in range(count[value]): 
             aList[index] = value
             index += 1
-    stop = time.clock()
-    return stop - start
+
+def countingSortTime(aList):
+    start = time.start()
+    countingSort(aList)
+    stop = time.stop()
+    return stop-start
+
+def countingSortMemory(aList):
+    p = psutil.Process(os.getpid())
+    countingSort(aList)
+    return p.memory_info().peak_wset
 
 
-def main():
-    mylist = [1, 4, 7, 2, 1, 3, 2, 1, 4, 2, 3, 2, 1]
-    mylist2 = list(mylist)
-    stepsList = [mylist]
-    countList = []
-    countingSortSteps(stepsList, countList)
-    
-    for each in stepsList:
-        print(each)
-
-    for each in countList:
-        print(each)
-
-    print()
-    print(countingSortTime(mylist2))
-    print(mylist2)
-main()
