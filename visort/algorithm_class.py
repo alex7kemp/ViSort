2f8d470ac2481a1a72b8f141ea8c242d512f288c
ThomasLalo <37088796+ThomasLalo@users.noreply.github.com>
2018-04-14 22:11:11 -0500

    Update algorithm_class.py

--- visort/algorithm_class.py
+++ visort/algorithm_class.py
@@ -11,27 +11,29 @@ class Algorithm_class:
         self.bubble_sorted = []
         self.bubble_steps = [self.myList]
         self.bubble_indices = []
-        self.bubble_psuedo = []
+        self.bubble_pseudo = []
 
         self.quick_sorted = []
         self.quick_steps = [self.myList]
         self.quick_indices = []
-        self.quick_psuedo = []
+        self.quick_pseudo = []
+        self.quickcomp = 0 #quick sort is recursive, so I just I didnt' want to mess with it's return statements
+        self.quickdata = 0 #quick sort is recursive, so I just I didnt' want to mess with it's return statements
 
         self.merge_sorted = []
         self.merge_steps = [self.myList]
         self.merge_indices = []
-        self.merge_psuedo = []
+        self.merge_pseudo = []
 
         self.counting_sorted = []
         self.counting_steps = [self.myList]
         self.counting_indices = []
-        self.counting_psuedo = []
+        self.counting_pseudo = []
 
         self.insertion_sorted = []
         self.insertion_steps = [self.myList]
         self.insertion_indices = []
-        self.insertion_psuedo = []
+        self.insertion_pseudo = []
 
     def bubble(self):
         placeholder = 1
@@ -111,6 +113,7 @@ class Algorithm_class:
 
     def quick_helper_visual(self, first, last):
         if first < last:
+            self.quick_pseudo.append("create a partition")
             splitpoint = self.quick_partition_visual(first, last)
 
             self.quick_helper_visual(first, splitpoint - 1)
@@ -244,10 +247,64 @@ class Algorithm_class:
         return stop - start
 
     def counting_visual(self):
-        placeholder = 1
+
+        changedIndices = [0, 0]
+        self.counting_sorted = list(self.myList)  # creating a copy of the list
+        maxplus = max(self.counting_sorted) + 1  # this is used to get the correct range of numbers to iterate through
+        count = [0] * maxplus  # prefilling count list
+
+        self.counting_pseudo.append("insert pseudocode step 1 here")
+        self.counting_steps.append(list(self.counting_sorted))
+        self.counting_indices.append(list(changedIndices))
+
+        for number in self.counting_sorted:
+            count[number] += 1  # creating count list
+
+            self.counting_pseudo.append("insert pseudocode step 2 here")
+            self.counting_steps.append(list(self.counting_sorted))
+            self.counting_indices.append(list(changedIndices))
+
+        index = 0
+        for value in range(maxplus):
+
+            self.counting_pseudo.append("insert pseudocode step 3 here")
+            self.counting_steps.append(list(self.counting_sorted))
+            self.counting_indices.append(list(changedIndices))
+
+            for number in range(count[value]):
+
+                self.counting_sorted[index] = value  # actual sorting
+
+                self.counting_pseudo.append("insert pseudocode step 4 here")
+                self.counting_steps.append(list(self.counting_sorted))  # creating steps list
+                if index > 0:  # creating index list
+                    changedIndices = [index - 1, index]
+                    self.counting_indices.append(list(changedIndices))
+                else:
+                    changedIndices = [0, 0]
+                    self.counting_indices.append(list(changedIndices))
+
+                index += 1  # this is for iterating through count list
+
+                self.counting_pseudo.append("insert pseudocode step 5 here")
+                self.counting_steps.append(list(self.counting_sorted))
+                self.counting_indices.append(list(changedIndices))
 
     def counting_comparisons(self):
-        placeholder = 1
+        comparisons = 0
+        data_movements = 0
+        maxplus = max(self.myList) + 1
+        count = [0] * maxplus
+        for number in self.myList:
+            count[number] += 1
+        index = 0
+        for value in range(maxplus):
+            for c in range(count[value]):
+                self.counting_sorted[index] = value
+                index += 1
+                comparisons += 1
+                data_movements += 1
+        return comparisons, data_movements
         
 
     def insertion(self):
